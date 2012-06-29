93d091bd74cc844ff9d3e0141660fcc213516539
Gorah <elon.aep.delin@gmail.com>
2012-06-29 16:56:15 +0200

    redesigned data flow between functions

--- update_DB.php
+++ update_DB.php
@@ -4,20 +4,31 @@ include "../common/db_connection_PDO.php";
 function run_update($inputData){
     switch($inputData['actionType']){
         case 'mig':
-            migUpdate($inputData['filePath']);
+            $funct_name = 'migUpdate';
             break;
     }
+    
+    $data = array('path' => $inputData['filePath'],
+                   'functName' => $funct_name);
+    getArray($data);
+    
 }
 
 
-function migUpdate($path){
+function getArray($inputData){
 //  file handle  pointing to uploaded file
-    $fh = fopen($path, "r");
+    $fh = fopen($inputData['path'], "r");
+    
     
 //  parsing csv file - each row is imported as an array  
     while (($data = fgetcsv($fh)) !== FALSE) {
-    print_r($data); //test operation
-    echo("<br/>");
+        call_user_func($inputData['functName'], $data);
+    }
+    
 }
+
+
+function migUpdate($array){
+//    receives a row of data in form of an array and processes it against DB
 }
 ?>
