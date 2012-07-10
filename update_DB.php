32ef0ccc80b00715eb0abe68e69023545f9272ca
Gorah <elon.aep.delin@gmail.com>
2012-07-10 14:35:25 +0200

    fixed - wrong file commited previously

--- update_DB.php
+++ update_DB.php
@@ -1,5 +1,8 @@
 <?php
 include "../common/db_connection_PDO.php";
+//include insert/update functions used by this tool to operate on DB
+include 'update_DB_insertf.php';
+include 'update_DB_udatef.php';
 
 //function that dispaches task to correct action related functions and prepares 
 //data for it to run. This is controller function for the whole update process
@@ -55,9 +58,37 @@ function migGetAction($array){
     
 }
 
-//**************************************
-//** Add New Employee to DB Section
-//**************************************
+//wrapper function that fires off all the updates one after another ensuring 
+//that all details are written to DB and are added in correct order
+function insert_NewEE($csv){
+
+    insertToEmployee($csv);
+    insertActions($csv);
+    insertActions($csv);
+    insertOpsMru($csv);
+    insertJobDetails($csv);
+    
+}
+
+function update_EE($arr){
+//    here add updating of the records for cost center, mru, job level/jobcode
+    $mru = check_MRU($arr);
+    if($mru == TRUE){
+        update_MRU($arr);
+    }
+    
+    
+    $cCenter = check_costCenter($arr);
+    
+    if($cCenter == TRUE){
+        update_CostCentr($arr);
+    }
+}
+
+
+//************************************
+//supporting functions - MIG
+//************************************
 
 function test_newHire($eeID){
 //    prepare SQL query to check if employee exist
@@ -85,132 +116,6 @@ function test_newHire($eeID){
     return $result;
 }
 
-//wrapper function that fires off all the updates one after another ensuring 
-//that all details are written to DB and are added in correct order
-function insert_NewEE($csv){
-
-    insertToEmployee($csv);
-    insertActions($csv);
-    insertActions($csv);
-    insertOpsMru($csv);
-    insertJobDetails($csv);
-    
-}
-
-//***********************************************
-//** Insert SQL functions for New Employee section
-//***********************************************
-
-function insertToEmployee($arr){
-    
-    $csv = $arr['data'];
-    //    prepare insert for main EE table
-    $SQL = "INSERT INTO tHR_Employee (ID, FirstName, LastName, Email) VALUES ("
-        . ":eeID, :name, :surN, :email);";
-    
-    //    run the query 
-    $dbh = DB_con();
-    $qry = $dbh->prepare($SQL);
-    $qry->bindParam(':eeID', $csv[3], PDO::PARAM_INT);
-    $qry->bindParam(':name', $csv[2], PDO::PARAM_STR);
-    $qry->bindParam(':surN', $csv[1], PDO::PARAM_STR);
-    $qry->bindParam(':email', $csv[4], PDO::PARAM_STR);
-    $qry->execute();
-    
-    unset($qry);
-    unset($dbh);
-}
-
-function insertActions($arr){
-    
-    $csv = $arr['data'];
-    //    prepare insert into Actions table
-    $SQL= "INSERT INTO tHR_Actions (EEID, ActionType, ReasonCode, StartDate, "
-        ."EndDate, ModifiedDate, ModifiedBy, EmploymentStatus) VALUES (:eeID, "
-        ."'New Hire', 'New Hire', '" .date('Y-m-d', strtotime($csv[12])) ."', "
-        ."'9999-12-31', '" .date("Y-m-d H:i:s", time()) ."', 'Mass Upload', :eStat);";
-    
-     //    run the query 
-    $dbh = DB_con();
-    $qry = $dbh->prepare($SQL);
-    $qry->bindParam(':eeID', $csv[3], PDO::PARAM_INT);
-    $qry->bindParam(':eStat', $csv[11], PDO::PARAM_STR);
-    $qry->execute();
-    
-    unset($qry);
-    unset($dbh);
-}
-
-function insertDate($arr){
-     $csv = $arr['data'];
-    //    insert original hire date into Date Specs table
-    $SQL = "INSERT INTO tHR_DateSpecs (EmpID, DateType, DateValue) VALUES (:eeID,"
-        ."'Original Hire Date', '" .date('Y-m-d', strtotime($csv[12])) . "');";
-     //    run the query 
-    $dbh = DB_con();
-    $qry = $dbh->prepare($SQL);
-    $qry->bindParam(':eeID', $csv[3], PDO::PARAM_INT);
-    $qry->execute();
-    
-    unset($qry);
-    unset($dbh);
-}
-
-function insertOpsMru($arr){
-     $csv = $arr['data'];
-    //   insert EE ID into OpsMRU table to identify which MRU employee belongs to 
-//   before he's added to his work group
-    $SQL = "INSERT INTO tHR_OpsMRU (ID, MRU) VALUES (:eeID, :mru);";
-    
-     //    run the query 
-    $dbh = DB_con();
-    $qry = $dbh->prepare($SQL);
-    $qry->bindParam(':eeID', $csv[3], PDO::PARAM_INT);
-    $qry->bindParam(':mru', $csv[8], PDO::PARAM_STR);
-    $qry->execute();
-    
-    unset($qry);
-    unset($dbh);
-}
-
-function insertJobDetails($arr){
-     $csv = $arr['data'];
-//    get info if employee is full time or part time
-     $fPT = time_scale($csv[13]);
-//    translate type of cotract from MIG ->BDW
-     $wCtr = contract_Type($csv[0]);
-     
-//   !!!!!!!!!!  add job level to DB, gui and to this query.!!!!!!!!!
-     
-//     prepare insert query for Job Datails table. NOTE: cost center format of 
-//     PLXXXXX... is used. To change it to CDXXXXX... format change $csv[7] to $csv[6]
-    $SQL = "INSERT INTO tHR_JobDetails (EEID, Project, WorkContractType, CostCenter"
-        .", JobCode, FTE, FullPartTime, ModifiedBy, StartDate, EndDate, ModifiedDate)"
-        ."VALUES (:eeID, :mru, :workC, :cCentr, :job, :fte, :fPT, 'Mass Upload', '" 
-        .date('Y-m-d', strtotime($csv[12])) ."', '9999-12-31', '" 
-        .date("Y-m-d H:i:s", time()) ."');";
-    
-      //    run the query 
-    $dbh = DB_con();
-    $qry = $dbh->prepare($SQL);
-    $qry->bindParam(':eeID', $csv[3], PDO::PARAM_INT);
-    $qry->bindParam(':mru', $csv[8], PDO::PARAM_STR);
-    $qry->bindParam(':workC', $wCtr, PDO::PARAM_STR);
-    $qry->bindParam(':cCentr', $csv[7], PDO::PARAM_STR);
-    $qry->bindParam(':job', $csv[10], PDO::PARAM_STR);
-    $qry->bindParam(':fte', $csv[13], PDO::PARAM_INT);
-    $qry->bindParam(':fPT', $fPT, PDO::PARAM_STR);
-    $qry->execute();
-    
-    unset($qry);
-    unset($dbh);
-}
-
-//***********************************************
-//** End of Insert SQL functions for New Employee section
-//***********************************************
-
-
 
 //test if employee is Full Time or Part Time, basing on his FTE
 function time_scale($FTE){
@@ -234,96 +139,6 @@ function contract_Type($val){
      
 }
 
-//**************************************
-//** END OF: Add New Employee to DB Section
-//**************************************
-
-function update_EE($arr){
-//    here add updating of the records for cost center, mru, job level/jobcode
-    $mru = check_MRU($arr);
-    if($mru == TRUE){
-        update_MRU($arr);
-    }
-    
-    
-    $cCenter = check_costCenter($arr);
-    
-    if($cCenter == TRUE){
-        update_CostCentr($arr);
-    }
-}
-
-
-function update_CostCentr($arr){
-//    function updating CC value for an employee
-    $csv = $arr['data'];
-    
-//    finding ID of the row to delimit
-    $SQL = "SELECT TOP 1 ID FROM tHR_JobDetails WHERE EEID = :eeid ORDER BY EndDate DESC";
-    $dbh = DB_con();
-    $qry = $dbh->prepare($SQL);
-    $qry->bindParam(':eeID', $csv[3], PDO::PARAM_INT);
-    $qry->execute();
-    
-    while($row = $qry->fetch(PDO::FETCH_NUM)){
-        $targetID = $row[0];
-    }
-    
-    unset($qry);
-    
-//    delimiting newest record for the employee
-    $SQL = "UPDATE tHR_JobDetails SET EndDate ='" .date("Y-m-d", time()) 
-            ."' WHERE ID = :tID";
-    $dbh = DB_con();
-    $qry = $dbh->prepare($SQL);
-    $qry->bindParam(':tID', $targetID, PDO::PARAM_INT);
-    $qry->execute();
-    
-//    inserting a new record with data from MIG. Note: this does mean that 
-//    details like job title, EElevel, provider are going to be left blank. 
-//    Inserting is done by re-using insert function
-    insertJobDetails($csv);
-    
-}
-
-function update_MRU($arr){
-//    function updating MRU value for an employee
-    $csv = $arr['data'];
-    
-//    finding ID of the row to delimit
-    $SQL = "SELECT TOP 1 ID FROM tHR_JobDetails WHERE EEID = :eeid ORDER BY EndDate DESC";
-    $dbh = DB_con();
-    $qry = $dbh->prepare($SQL);
-    $qry->bindParam(':eeID', $csv[3], PDO::PARAM_INT);
-    $qry->execute();
-    
-    while($row = $qry->fetch(PDO::FETCH_NUM)){
-        $targetID = $row[0];
-    }
-    
-    unset($qry);
-    
-//    delimiting newest record for the employee
-    $SQL = "UPDATE tHR_JobDetails SET EndDate ='" .date("Y-m-d", time()) 
-            ."' WHERE ID = :tID";
-    $dbh = DB_con();
-    $qry = $dbh->prepare($SQL);
-    $qry->bindParam(':tID', $targetID, PDO::PARAM_INT);
-    $qry->execute();
-    
-//    inserting a new record with data from MIG. Note: this does mean that 
-//    details like job title, EElevel, provider are going to be left blank. 
-//    Inserting is done by re-using insert function
-    insertJobDetails($csv);
-    
-}
-
-
-//************************************
-//supporting functions
-//************************************
-
-
 //function testing whether Cost Center for employee is up to date
 function check_costCenter($arr){
     $csv = $arr['data'];
@@ -342,7 +157,7 @@ function check_costCenter($arr){
     
     if(hits == 0){
         $SQL = "SELECT Count(EEID) FROM tHR_JobDetails WHERE EEID = :eeID AND"
-            ." CostCenter = :cCentr AND EndDate > '" .date("Y-m-d", time()) ."'"
+            ." CostCenter = :cCentr AND EndDate > '" .date("Y-m-d", $arr['timeStamp']) ."'"
             ." AND StartDate < '" .date("Y-m-d", $arr['timeStamp']) ."'";
     
         $dbh = DB_con();
@@ -389,7 +204,7 @@ function check_MRU($arr){
     
     if(hits == 0){
         $SQL = "SELECT Count(EEID) FROM tHR_JobDetails WHERE EEID = :eeID AND"
-            ." Project = :MRU AND EndDate > '" .date("Y-m-d", time()) ."'"
+            ." Project = :MRU AND EndDate > '" .date("Y-m-d", $arr['timeStamp']) ."'"
             ." AND StartDate < '" .date("Y-m-d", $arr['timeStamp']) ."'";
     
         $dbh = DB_con();
